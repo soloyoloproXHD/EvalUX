5f81a6d2ef988f2c6696c6fdfda365eba9c4542a
Hiram Cornejo <hiramcornejo10@gmail.com>
2024-10-09 13:36:08 -0600

    Input 100% dinamico

--- appux/src/components/ui/inputOuside.tsx
+++ appux/src/components/ui/inputOuside.tsx
@@ -1,23 +1,46 @@
 import { Input } from "@nextui-org/react"
-
+import { useState } from "react";
+import { EyeSlashFilledIcon } from "./EyeSlashFilledIcon";
+import { EyeFilledIcon } from "./EyeFilledIcon";
 interface InputProps {
     type: string;
+    size?: "sm" | "md" | "lg";
     label: string;
-    name: string;
-    value: string;
-    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+    name?: string;
+    value?: string;
+    onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+    isPassword?: boolean;
 }
 
-export default function AppInputOut({type, label, name, value, onChange}: InputProps) {
+export default function AppInputOut({type, size, label, name, value, onChange, isPassword = false}: InputProps) {
+    const [isVisible, setIsVIsible] = useState(false);
+
+    const toggleVisibility = () => setIsVIsible((prev) => !prev);
     return (
         <>
             <Input key={"outside"} 
-                   type={type} 
+                   type={type && isVisible ? 'text' : type}
+                   size={size}
                    label={label}
                    name={name}
                    value={value}
                    onChange={onChange} 
-                   labelPlacement="outside"/>
+                   labelPlacement="outside"
+                   endContent={isPassword && (
+                        <button
+                            type="button"
+                            onClick={toggleVisibility}
+                            aria-label="toggle password visibility"
+                            className="focus:outline-none"
+                        >
+                        {isVisible ? (
+                            <EyeSlashFilledIcon/>
+                        ) : (
+                            <EyeFilledIcon/>
+                        )}
+                    </button>
+                )}
+            />
         </>
     )
 }
--- appux/src/components/ui/modalRegister.tsx
+++ appux/src/components/ui/modalRegister.tsx
@@ -62,18 +62,15 @@ export default function AppModalR({show, onClose}: ModalProps) {
                         <div className="row-span-2">
                             <p className="text-center p-5">Crear Cuenta</p>
                             <div className="gap-1">
-                                <AppInputOut type="" label="Nombres" name="nombres" value={formData.nombres} onChange={handleChange}/>
-                                <AppInputOut type="" label="Apellidos" name="apellidos" value={formData.apellidos} onChange={handleChange}/>
-                                <AppInputOut type="email" label="Correo Electronico" name="correoE" value={formData.correoE} onChange={handleChange}/>
-                                <AppInputOut type="password" label="Contraseña" name="contrasena" value={formData.contrasena} onChange={handleChange}/>
-                                <AppInputOut type="password" label="Contraseña" name="ccontrasena" value={formData.ccontrasena} onChange={handleChange}/>
+                                <AppInputOut type="text" size="md" label="Nombres" name="nombres" value={formData.nombres} onChange={handleChange}/>
+                                <AppInputOut type="text" size="md" label="Apellidos" name="apellidos" value={formData.apellidos} onChange={handleChange}/>
+                                <AppInputOut type="email" size="md" label="Correo Electronico" name="correoE" value={formData.correoE} onChange={handleChange}/>
+                                <AppInputOut type="password" size="md" label="Contraseña" name="contrasena" value={formData.contrasena} onChange={handleChange} isPassword={true}/>
+                                <AppInputOut type="password" size="md" label="Repita Contraseña" name="ccontrasena" value={formData.ccontrasena} onChange={handleChange} isPassword={true}/>
                             </div>
-                            
                         </div>
                         <div >3</div>
-                            
                     </div>
-                    
                 </ModalHeader>
                 <ModalBody>
                   
