f262c5165c341e7b460609459128ca1e0188e786
Hiram Cornejo <hiramcornejo10@gmail.com>
2024-10-10 17:21:34 -0600

    modal registro update and InputOutside completed

--- appux/src/components/ui/inputOuside.tsx
+++ appux/src/components/ui/inputOuside.tsx
@@ -1,5 +1,5 @@
 import { Input } from "@nextui-org/react"
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import { EyeSlashFilledIcon } from "./EyeSlashFilledIcon";
 import { EyeFilledIcon } from "./EyeFilledIcon";
 interface InputProps {
@@ -10,12 +10,27 @@ interface InputProps {
     value?: string;
     onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
     isPassword?: boolean;
+    isInvalid?: boolean;
+    errorMessage?: string;
 }
 
-export default function AppInputOut({type, size, label, name, value, onChange, isPassword = false}: InputProps) {
+export default function AppInputOut({type, size, label, name, value = "", onChange, isPassword = false, isInvalid = false, errorMessage = ""}: InputProps) {
     const [isVisible, setIsVIsible] = useState(false);
 
     const toggleVisibility = () => setIsVIsible((prev) => !prev);
+
+    //Validación del Correo
+    const validateEmail = (value: string) => 
+        value.match(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i);
+    
+    const isEmailInvalid = useMemo(() => {
+        if (type === "email" && value !== "") {
+          return !validateEmail(value);
+        }
+        return false;
+      }, [value, type]);
+
+
     return (
         <>
             <Input key={"outside"} 
@@ -26,6 +41,9 @@ export default function AppInputOut({type, size, label, name, value, onChange, i
                    value={value}
                    onChange={onChange} 
                    labelPlacement="outside"
+                   isInvalid={isInvalid || isEmailInvalid}
+                   color={isInvalid || isEmailInvalid ? "danger" : "default"}
+                   errorMessage={isEmailInvalid ? "Por favor ingrese un correo válido" : errorMessage}
                    endContent={isPassword && (
                         <button
                             type="button"
--- appux/src/components/ui/modalRegister.tsx
+++ appux/src/components/ui/modalRegister.tsx
@@ -2,6 +2,10 @@
 import React, { useEffect, useState } from "react";
 import {Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, useDisclosure} from "@nextui-org/react";
 import AppInputOut from "./inputOuside";
+import Image from "next/image";
+import { useTheme } from 'next-themes';
+import Logo from '../../../public/img/Logo.png';
+import LogoW from '../../../public/img/Logo.png';
 
 
 interface ModalProps {
@@ -11,6 +15,7 @@ interface ModalProps {
 
 export default function AppModalR({show, onClose}: ModalProps) {
     const {isOpen,onOpen} = useDisclosure();
+    const { theme } = useTheme();
 
     useEffect(() => {
         if (!show) return;
@@ -19,8 +24,6 @@ export default function AppModalR({show, onClose}: ModalProps) {
         } else {
             onClose();
         }
-
-        
     }, [show, onOpen, onClose]);
 
     const handleCloseModal = () => {
@@ -35,38 +38,138 @@ export default function AppModalR({show, onClose}: ModalProps) {
         ccontrasena: ''
     });
 
+    const [errors, setErrors] = useState({
+      nombres: '',
+      apellidos: '',
+      correoE: '',
+      contrasena: '',
+      ccontrasena: ''
+    });
+
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const {name, value} = e.target;
-        setFormData({
-            ...formData,
-            [name]: value
+      const {name, value} = e.target;
+      setFormData({
+          ...formData,
+          [name]: value
+      });
+
+      //Quita el error al comenzar a escribir
+      if (value.trim() !== ""){
+        setErrors({
+          ...errors,
+          [name]: ""
         });
+      }
     };
 
+    const validateForm = () => {
+      const newErrors = {...errors};
+      let isValid = true;
+
+      if (formData.nombres.trim() === '') {
+        newErrors.nombres = "El campo de nombres no puede estar vacío";
+        isValid = false;
+      }
+      if (formData.apellidos.trim() === '') {
+        newErrors.apellidos = "El campo de apellidos no puede estar vacío";
+        isValid = false;
+      }
+      if (formData.correoE.trim() === '') {
+        newErrors.correoE = "El campo de correo no puede estar vacío";
+        isValid = false;
+      }
+      if (formData.contrasena.trim() === '') {
+        newErrors.contrasena = "El campo de contraseña no puede estar vacío";
+        isValid = false;
+      }
+      if (formData.ccontrasena.trim() === '') {
+        newErrors.ccontrasena = "El campo de repetir contraseña no puede estar vacío";
+        isValid = false;
+      }
+  
+      setErrors(newErrors);
+      return isValid;
+
+    }
+
     const handleSubmit = (e: React.FormEvent) => {
-        e.preventDefault();
-        console.log(formData)
-        handleCloseModal()
+      e.preventDefault();
+      
+      if (validateForm()) {
+        handleCloseModal();
+      } else {
+        console.log("Formulario Inválido");
+      }
     };
 
     if (!show) return null;
 
     return (
       <>
-        <Modal backdrop={"blur"} isOpen={isOpen} onClose={handleCloseModal}>
+        <Modal size="xl" backdrop={"blur"} isOpen={isOpen} onClose={handleCloseModal}>
           <ModalContent>
               <>
                 <ModalHeader className="flex flex-col gap-1">
-                    <div className="grid grid-cols-2 grid-rows-2 gap-5">
-                        <div >logo</div>
-                        <div className="row-span-2">
-                            <p className="text-center p-5">Crear Cuenta</p>
-                            <div className="gap-1">
-                                <AppInputOut type="text" size="md" label="Nombres" name="nombres" value={formData.nombres} onChange={handleChange}/>
-                                <AppInputOut type="text" size="md" label="Apellidos" name="apellidos" value={formData.apellidos} onChange={handleChange}/>
-                                <AppInputOut type="email" size="md" label="Correo Electronico" name="correoE" value={formData.correoE} onChange={handleChange}/>
-                                <AppInputOut type="password" size="md" label="Contraseña" name="contrasena" value={formData.contrasena} onChange={handleChange} isPassword={true}/>
-                                <AppInputOut type="password" size="md" label="Repita Contraseña" name="ccontrasena" value={formData.ccontrasena} onChange={handleChange} isPassword={true}/>
+                  <div className="grid grid-cols-2 grid-rows-2 gap-5">
+                    <div className="flex place-content-center items-center gap-2 text-white">
+                      <Image src={theme === 'dark' ? LogoW : Logo} alt="Logo" className='h-7 w-auto' />
+                      <p className='text-lg font-semibold'>EvalUX</p>
+                    </div>
+                    <div className="row-span-2">
+                        <p className="text-center p-5">Crear Cuenta</p>
+                        <div className="gap-1">
+                                <AppInputOut 
+                                  type="text" 
+                                  size="md" 
+                                  label="Nombres" 
+                                  name="nombres" 
+                                  value={formData.nombres} 
+                                  onChange={handleChange}
+                                  isInvalid = {!!errors.nombres}
+                                  errorMessage={errors.nombres}
+                                />
+                                <AppInputOut 
+                                  type="text" 
+                                  size="md" 
+                                  label="Apellidos" 
+                                  name="apellidos" 
+                                  value={formData.apellidos} 
+                                  onChange={handleChange}
+                                  isInvalid = {!!errors.apellidos}
+                                  errorMessage={errors.apellidos}
+                                />
+                                <AppInputOut 
+                                  type="email" 
+                                  size="md" 
+                                  label="Correo Electronico" 
+                                  name="correoE" 
+                                  value={formData.correoE} 
+                                  onChange={handleChange}
+                                  isInvalid = {!!errors.correoE}
+                                  errorMessage={errors.correoE}
+                                />
+                                <AppInputOut 
+                                  type="password" 
+                                  size="md" 
+                                  label="Contraseña" 
+                                  name="contrasena" 
+                                  value={formData.contrasena} 
+                                  onChange={handleChange}
+                                  isInvalid = {!!errors.contrasena}
+                                  errorMessage={errors.contrasena}
+                                  isPassword={true}
+                                />
+                                <AppInputOut 
+                                  type="password" 
+                                  size="md" 
+                                  label="Repita Contraseña" 
+                                  name="ccontrasena" 
+                                  value={formData.ccontrasena} 
+                                  onChange={handleChange}
+                                  isInvalid = {!!errors.ccontrasena}
+                                  errorMessage={errors.ccontrasena}
+                                  isPassword={true}
+                                />
                             </div>
                         </div>
                         <div >3</div>
