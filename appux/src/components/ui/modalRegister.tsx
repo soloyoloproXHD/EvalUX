5f751af2a097c5fdc6cc9592997379980705c6ca
Hiram Abif Cornejo Cornejo <hiramcornejo10@gmail.com>
2024-10-10 20:15:01 -0600

    Modal de registro completo
    EL modal para registro está completo, lo detallaré por completo al finalizar el software

--- appux/src/components/ui/modalRegister.tsx
+++ appux/src/components/ui/modalRegister.tsx
@@ -1,194 +1,238 @@
 'use client';
 import React, { useEffect, useState } from "react";
-import {Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, useDisclosure} from "@nextui-org/react";
+import { Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, useDisclosure } from "@nextui-org/react";
+import { faArrowRightToBracket, faUserPlus } from '@fortawesome/free-solid-svg-icons';
+import { Divider } from "@nextui-org/divider";
 import AppInputOut from "./inputOuside";
 import Image from "next/image";
 import { useTheme } from 'next-themes';
 import Logo from '../../../public/img/Logo.png';
 import LogoW from '../../../public/img/Logo.png';
+import AdaptButton from "../AdaptButton";
 
 
 interface ModalProps {
-    show: boolean;
-    onClose: () => void;
+  show: boolean;
+  onClose: () => void;
 }
 
-export default function AppModalR({show, onClose}: ModalProps) {
-    const {isOpen,onOpen} = useDisclosure();
-    const { theme } = useTheme();
+export default function AppModalR({ show, onClose }: ModalProps) {
+  const { isOpen, onOpen } = useDisclosure();
+  const { theme } = useTheme();
 
-    useEffect(() => {
-        if (!show) return;
-        else if (show == true){
-            onOpen();
-        } else {
-            onClose();
-        }
-    }, [show, onOpen, onClose]);
+  useEffect(() => {
+    if (!show) return;
+    else if (show == true) {
+      onOpen();
+    } else {
+      onClose();
+    }
+  }, [show, onOpen, onClose]);
 
-    const handleCloseModal = () => {
-        onClose();
-    };
+  const handleCloseModal = () => {
+    onClose();
+  };
 
-    const [formData, setFormData] = useState({
-        nombres: '',
-        apellidos: '',
-        correoE: '',
-        contrasena: '',
-        ccontrasena: ''
+  const initialFormData = {
+    nombres: '',
+    apellidos: '',
+    correoE: '',
+    contrasena: '',
+    ccontrasena: ''
+  };
+
+  const [formData, setFormData] = useState({
+    nombres: '',
+    apellidos: '',
+    correoE: '',
+    contrasena: '',
+    ccontrasena: ''
+  });
+
+  const [errors, setErrors] = useState({
+    nombres: '',
+    apellidos: '',
+    correoE: '',
+    contrasena: '',
+    ccontrasena: ''
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value
     });
 
-    const [errors, setErrors] = useState({
-      nombres: '',
-      apellidos: '',
-      correoE: '',
-      contrasena: '',
-      ccontrasena: ''
-    });
-
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-      const {name, value} = e.target;
-      setFormData({
-          ...formData,
-          [name]: value
+    //Quita el error al comenzar a escribir
+    if (value.trim() !== "") {
+      setErrors({
+        ...errors,
+        [name]: ""
       });
+    }
+  };
 
-      //Quita el error al comenzar a escribir
-      if (value.trim() !== ""){
-        setErrors({
-          ...errors,
-          [name]: ""
-        });
-      }
-    };
+  const validateForm = () => { //validador del formulario
+    const newErrors = { ...errors };
+    let isValid = true;
 
-    const validateForm = () => {
-      const newErrors = {...errors};
-      let isValid = true;
-
-      if (formData.nombres.trim() === '') {
-        newErrors.nombres = "El campo de nombres no puede estar vacío";
-        isValid = false;
-      }
-      if (formData.apellidos.trim() === '') {
-        newErrors.apellidos = "El campo de apellidos no puede estar vacío";
-        isValid = false;
-      }
-      if (formData.correoE.trim() === '') {
-        newErrors.correoE = "El campo de correo no puede estar vacío";
-        isValid = false;
-      }
-      if (formData.contrasena.trim() === '') {
-        newErrors.contrasena = "El campo de contraseña no puede estar vacío";
-        isValid = false;
-      }
-      if (formData.ccontrasena.trim() === '') {
-        newErrors.ccontrasena = "El campo de repetir contraseña no puede estar vacío";
-        isValid = false;
-      }
-  
-      setErrors(newErrors);
-      return isValid;
-
+    if (formData.nombres.trim() === '') {
+      newErrors.nombres = "El campo de nombres no puede estar vacío";
+      isValid = false;
+    }
+    if (formData.apellidos.trim() === '') {
+      newErrors.apellidos = "El campo de apellidos no puede estar vacío";
+      isValid = false;
+    }
+    if (formData.correoE.trim() === '') {
+      newErrors.correoE = "El campo de correo no puede estar vacío";
+      isValid = false;
+    }
+    if (formData.contrasena.trim() === '') {
+      newErrors.contrasena = "El campo de contraseña no puede estar vacío";
+      isValid = false;
+    }
+    if (formData.ccontrasena.trim() === '') {
+      newErrors.ccontrasena = "El campo de repetir contraseña no puede estar vacío";
+      isValid = false;
+    }
+    if (formData.contrasena !== formData.ccontrasena) {
+      newErrors.ccontrasena = "Las contraseñas no coinciden";
+      isValid = false;
     }
 
-    const handleSubmit = (e: React.FormEvent) => {
-      e.preventDefault();
-      
-      if (validateForm()) {
-        handleCloseModal();
-      } else {
-        console.log("Formulario Inválido");
-      }
-    };
+    setErrors(newErrors);
+    return isValid;
 
-    if (!show) return null;
-
-    return (
-      <>
-        <Modal size="xl" backdrop={"blur"} isOpen={isOpen} onClose={handleCloseModal}>
-          <ModalContent>
-              <>
-                <ModalHeader className="flex flex-col gap-1">
-                  <div className="grid grid-cols-2 grid-rows-2 gap-5">
-                    <div className="flex place-content-center items-center gap-2 text-white">
-                      <Image src={theme === 'dark' ? LogoW : Logo} alt="Logo" className='h-7 w-auto' />
-                      <p className='text-lg font-semibold'>EvalUX</p>
-                    </div>
-                    <div className="row-span-2">
-                        <p className="text-center p-5">Crear Cuenta</p>
-                        <div className="gap-1">
-                                <AppInputOut 
-                                  type="text" 
-                                  size="md" 
-                                  label="Nombres" 
-                                  name="nombres" 
-                                  value={formData.nombres} 
-                                  onChange={handleChange}
-                                  isInvalid = {!!errors.nombres}
-                                  errorMessage={errors.nombres}
-                                />
-                                <AppInputOut 
-                                  type="text" 
-                                  size="md" 
-                                  label="Apellidos" 
-                                  name="apellidos" 
-                                  value={formData.apellidos} 
-                                  onChange={handleChange}
-                                  isInvalid = {!!errors.apellidos}
-                                  errorMessage={errors.apellidos}
-                                />
-                                <AppInputOut 
-                                  type="email" 
-                                  size="md" 
-                                  label="Correo Electronico" 
-                                  name="correoE" 
-                                  value={formData.correoE} 
-                                  onChange={handleChange}
-                                  isInvalid = {!!errors.correoE}
-                                  errorMessage={errors.correoE}
-                                />
-                                <AppInputOut 
-                                  type="password" 
-                                  size="md" 
-                                  label="Contraseña" 
-                                  name="contrasena" 
-                                  value={formData.contrasena} 
-                                  onChange={handleChange}
-                                  isInvalid = {!!errors.contrasena}
-                                  errorMessage={errors.contrasena}
-                                  isPassword={true}
-                                />
-                                <AppInputOut 
-                                  type="password" 
-                                  size="md" 
-                                  label="Repita Contraseña" 
-                                  name="ccontrasena" 
-                                  value={formData.ccontrasena} 
-                                  onChange={handleChange}
-                                  isInvalid = {!!errors.ccontrasena}
-                                  errorMessage={errors.ccontrasena}
-                                  isPassword={true}
-                                />
-                            </div>
-                        </div>
-                        <div >3</div>
-                    </div>
-                </ModalHeader>
-                <ModalBody>
-                  
-                </ModalBody>
-                <ModalFooter>
-                  <Button onClick={handleCloseModal} color="danger" variant="light" onPress={handleCloseModal}>
-                    Close
-                  </Button>
-                  <Button onClick={handleSubmit} color="primary" >
-                    Action
-                  </Button>
-                </ModalFooter>
-              </>
-          </ModalContent>
-        </Modal>
-      </>
-    );
   }
+
+  //Validación del Correo
+  const validateEmail = (value: string) => 
+    value.match(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i);
+
+  const handleSubmit = (e: React.FormEvent) => { //manejador del formData
+    e.preventDefault();
+
+    if (validateForm()) {
+      // Validar el formato del correo electrónico
+      if (!validateEmail(formData.correoE)) {
+        setErrors(prev => ({
+          ...prev,
+          correoE: "Por favor ingrese un correo válido"
+        }));
+        return;
+      }
+
+      console.log(formData) //Manejar aquí el envio de los datos al backend
+      setFormData(initialFormData); //Limpieza de los inputs si el formulario es valido
+      handleCloseModal(); //Cerrado del modal
+    } else {
+      console.log("Formulario Inválido");
+    }
+  };
+
+  if (!show) return null;
+
+  return (
+    <>
+      <Modal size="xl" backdrop={"blur"} isOpen={isOpen} onClose={handleCloseModal}>
+        <ModalContent>
+          <>
+            <ModalHeader className="flex flex-col gap-1">
+              <div className="grid grid-cols-2 grid-rows-1 gap-5">
+                <div>
+                  <div className="flex place-content-center items-center gap-2 mt-4 text-white">
+                    <Image src={theme === 'dark' ? LogoW : Logo} alt="Logo" className='h-12 w-auto' />
+                    <p className='text-2xl font-semibold'>EvalUX</p>
+                  </div>
+                </div>
+                <div>
+                  <p className="text-2xl text-center p-5">Crear Cuenta</p>
+                </div>
+              </div>
+            </ModalHeader>
+            <ModalBody>
+              <div className="grid grid-cols-2 grid-rows-1 gap-5">
+                <div className="flex-1 items-center place-content-center">
+                  <div className="flex items-center">
+                    <Divider className="max-w-14" />
+                    <p className="text-center text-sm">¿Ya tienes una cuenta?</p>
+                    <Divider className="max-w-14" />
+                  </div>
+                  <div className="flex place-content-center items-center p-5">
+                    <AdaptButton icon={faArrowRightToBracket} texto="Iniciar Sesión" />
+                  </div>
+                </div>
+                <div>
+                  <div className="gap-1">
+                    <AppInputOut
+                      type="text"
+                      size="md"
+                      label="Nombres"
+                      name="nombres"
+                      value={formData.nombres}
+                      onChange={handleChange}
+                      isInvalid={!!errors.nombres}
+                      errorMessage={errors.nombres}
+                    />
+                    <AppInputOut
+                      type="text"
+                      size="md"
+                      label="Apellidos"
+                      name="apellidos"
+                      value={formData.apellidos}
+                      onChange={handleChange}
+                      isInvalid={!!errors.apellidos}
+                      errorMessage={errors.apellidos}
+                    />
+                    <AppInputOut
+                      type="email"
+                      size="md"
+                      label="Correo Electronico"
+                      name="correoE"
+                      value={formData.correoE}
+                      onChange={handleChange}
+                      isInvalid={!!errors.correoE}
+                      errorMessage={errors.correoE}
+                    />
+                    <AppInputOut
+                      type="password"
+                      size="md"
+                      label="Contraseña"
+                      name="contrasena"
+                      value={formData.contrasena}
+                      onChange={handleChange}
+                      isInvalid={!!errors.contrasena}
+                      errorMessage={errors.contrasena}
+                      isPassword={true}
+                    />
+                    <AppInputOut
+                      type="password"
+                      size="md"
+                      label="Confirmar Contraseña"
+                      name="ccontrasena"
+                      value={formData.ccontrasena}
+                      onChange={handleChange}
+                      isInvalid={!!errors.ccontrasena}
+                      errorMessage={errors.ccontrasena}
+                      isPassword={true}
+                    />
+                  </div>
+                  <p className="text-sm text-center p-3">
+                    Al crear una cuenta, aceptas las Condiciones de uso y el Aviso de Privacidad de EvalUX.
+                  </p>
+                  <div className="flex place-content-center items-center">
+                    <AdaptButton icon={faUserPlus} onClick={handleSubmit} texto="Continuar" />
+                  </div>
+                </div>
+              </div>
+            </ModalBody>
+            <ModalFooter>
+            </ModalFooter>
+          </>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+}
