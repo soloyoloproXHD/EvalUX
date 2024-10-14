ecf50d1a97ff51c16bb02bc93276bd4520381953
Hiram Cornejo <hiramcornejo10@gmail.com>
2024-10-14 10:19:34 -0600

    db updateURL

--- appux/src/utils/db.ts
+++ appux/src/utils/db.ts
@@ -5,11 +5,7 @@ let conn: Pool | null = null;
 
 if(!conn){
     conn = new Pool({
-        user: process.env.DATABASE_USER,
-        password: process.env.DATABASE_PASSWORD,
-        host: process.env.DATABASE_HOST,
-        port: process.env.DATABASE_PORT ? parseInt(process.env.DATABASE_PORT, 10): undefined,
-        database: process.env.DATABASE_NAME
+        connectionString: process.env.POSTGRES_URL,
     })
 }
 
